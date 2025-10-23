573586e70db4cdee90711b15cb6e42fb5a7ab2fb
Bkeefe901 <bradyjokeefe@gmail.com>
2025-10-23 11:22:16 -0400

    Added password strength checking

--- src/components/AuthForms/RegisterForm.jsx
+++ src/components/AuthForms/RegisterForm.jsx
@@ -4,38 +4,66 @@ import { useNavigate } from "react-router-dom";
 import style from './Forms.module.css';
 
 export default function RegisterForm({ setNewUser }) {
-    const { signUp } = useAuth();
-    const nav = useNavigate();
-    const [formData, setFormData] = useState({
-        userName: "",
-        email: "",
-        password: "",
-        password2: "",
-    });
+  const { signUp } = useAuth();
+  const nav = useNavigate();
+  const [strength, setStrength] = useState(null); // state for password strength
+  const [strengthStyle, setStrengthStyle] = useState(""); // state to style div color based on password strength
+  const regexStr = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).*$/;
+  const regexWeak = /^(?=.*[a-zA-Z])(?=.*\d).*$/;
+  const [formData, setFormData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-    function handleChange(e) {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+  // Message variables for password strength
+  const strong = 'Password is strong';
+  const medium = 'Password Strength is medium, it contains letters and numbers';
+  const weak = 'Password is weak';
+
+
+
+  function handleChange(e) {
+
+    // Conditionals for password strength
+    if (e.target.name == 'password') {
+      if (regexStr.test(e.target.value) && e.target.value.length >= 6) {
+        setStrength(strong);
+        setStrengthStyle('green');
+      } else if (regexWeak.test(e.target.value) && e.target.value.length >= 6) {
+        setStrength(medium);
+        setStrengthStyle('yellow');
+      } else {
+        setStrength(weak);
+        setStrengthStyle('red');
+      }
     }
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        try {
-            if (formData.password !== formData.password2){
-                alert("Passwords do not match");
-                throw new Error("Password Dont Match");
-            }
-            await signUp(formData);
-            nav("/dash");
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
 
-    const handleClick = () => {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      if (formData.password !== formData.password2) {
+        alert("Passwords do not match");
+        throw new Error("Password Dont Match");
+      }
+      await signUp(formData);
+      nav("/dash");
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  // Handler to toggle to login form
+  const handleClick = () => {
     setNewUser(false);
   };
 
   return (
+    <>
     <div className={style.forms}>
       <h2>Register</h2>
       <form onSubmit={handleSubmit}>
@@ -85,6 +113,10 @@ export default function RegisterForm({ setNewUser }) {
         Already have an account? <button onClick={handleClick}>Sign In</button>
       </p>
     </div>
+    <div style={{ backgroundColor: strengthStyle, color: 'black', width: '400px', margin: 'auto'}} >
+      <h3>{strength}</h3>
+    </div>
+    </>
   );
 
 }
